2bf0d04978f570a5319bea1ce7c2e32eed062552
Chris N <hi.chrisn@gmail.com>
2022-02-17 15:15:39 -0300

    sorting paginated data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -5,18 +5,17 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100, // flight_number
-  mission: 'Kepler Exploration X', // name
-  rocket: 'Explorer IS1', // rocket.name
-  launchDate: new Date('December 27, 2030'), // date_local
-  target: 'Kepler-442 b', // not applicable
-  customers: ['ZTM', 'NASA'], // payload.customers for each payload
-  upcoming: true, // upcoming
-  success: true, // success
-};
-
-saveLaunch(launch);
+// const launch = {
+//   flightNumber: 100, // flight_number
+//   mission: 'Kepler Exploration X', // name
+//   rocket: 'Explorer IS1', // rocket.name
+//   launchDate: new Date('December 27, 2030'), // date_local
+//   target: 'Kepler-442 b', // not applicable
+//   customers: ['ZTM', 'NASA'], // payload.customers for each payload
+//   upcoming: true, // upcoming
+//   success: true, // success
+// };
+// saveLaunch(launch);
 
 const SPACEX_API_URL = 'https://api.spacexdata.com/v4/launches/query';
 
@@ -111,6 +110,7 @@ async function getLatestFlightNumber() {
 async function getAllLaunches(skip, limit) {
   return await launchesDatabase
   .find({}, { '_id': 0, '__v': 0 })
+  .sort({ flightNumber: 1 })
   .skip(skip)
   .limit(limit);
 }
